59758091d8d31b29b5ac6fa75541f2142d7db1f6
john <johnbabu021@gmail.com>
2021-08-26 08:05:35 +0530

    oveflow changed

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,9 +2,10 @@ import React from 'react'
 import About from '../app/components/About/About'
 import Banner from '../app/components/Banner/Banner'
 import Project from '../app/components/Projects/Project'
+import './Home.css'
 function Portfolio() {
     return (
-        <div>
+        <div className="portfolio">
             <Banner />
             <About />
             <Project />
