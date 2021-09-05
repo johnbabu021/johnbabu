9090e699d023e1af91c1a80ac0eeee2cb2e90d3f
john <johnbabu021@gmail.com>
2021-09-05 13:34:22 +0530

    added resume

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,6 +4,11 @@ import './Banner.css'
 
 function Banner() {
 
+    const pdfOpen = () => {
+        window.open("/pdf/pdfresume.pdf");
+        return true
+    }
+
     return (
         <div className="banner">
             <div className="about__portfolio">
@@ -13,7 +18,7 @@ function Banner() {
 
                 <div className="mail__send">
                     <a className="get__btn" target="_blank" rel="noreferrer" href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=johnbabu021@gmail.com">Get In Touch</a>
-                    <button className="get__btn resume">
+                    <button className="get__btn resume" onClick={pdfOpen}>
                         Resume
                     </button>
 
