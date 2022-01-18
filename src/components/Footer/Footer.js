2cd464679161ae0e1417480c1ac6d20f5ab97fe3
johnbabu021 <johnbabu021@gmail.com>
2022-01-18 21:32:15 +0530

    feat:added twitter

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import { GitHub, Instagram, LinkedIn } from '@material-ui/icons'
+import { GitHub, Twitter, LinkedIn } from '@material-ui/icons'
 import React from 'react'
 import './Footer.css'
 
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <div className="footer">
             <div className="footer__left">
-                <a href="https://www.instagram.com/_.joh.n/"><Instagram /></a>
+                <a href="https://twitter.com/johnbabu021"><Twitter /></a>
                 <a href="https://www.linkedin.com/in/john-babu-3931a319a/"><LinkedIn /></a>
                 <a href="https://github.com/johnbabu021"><GitHub /></a>
             </div>
